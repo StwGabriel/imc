1728d0dcab95015677e28bab4dadb5c539b3a19b
Gabriel Silva <gabbrielsilvactt@gmail.com>
2021-03-25 15:18:38 -0300

    adicionando desk V

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,54 +1,37 @@
 //
-
-const resultScreen = document.querySelector('.resultScreen');
-function openResultScreen() {
-
-   resultScreen.classList.add('onScreen');
-}
-function closeResultScreen() {
-
-   resultScreen.classList.remove('onScreen');
-}
+const width = window.matchMedia("(min-width: 768px)");
 
 function imcCalculator() {
 
-   console.log('run');
+   const prevWeight = document.getElementById('weight').value,
+      prevHeight = document.getElementById('height').value;
+   const weight = Number(prevWeight),
+      height = Number(prevHeight) / 100;
 
-   const prevWeight = document.getElementById('weight').value;
-   const prevHeight = document.getElementById('height').value;
-
-   const weight = Number(prevWeight);
-   const height = Number(prevHeight) / 100;
-
-   const alert = document.querySelector('.alert')
-
-   if (weight && height === '0' || '') {
+   if (weight === 0 || height === 0) {
 
       return console.log('false');
    } else {
 
-      console.log('its true');
-
       const imcResult = weight / (height * height);
-      console.log(weight, height);
-      console.log(imcResult);
 
       function result() {
 
          if (imcResult <= 18.5) {
-
             return `
-
-               <div style=' background: #dbb61233;color: #dbb612; ' class="resultData">
+               <div
+                  class=" resultAnim resultData"
+                  style='background: #dbb61233;color: #dbb612;'
+                  >
                    <p>${imcResult.toFixed(2)}</p>
                   <span>Abaixo do peso</span>
                </div>
             `
          } else if (imcResult >= 18.6 && imcResult <= 24.9) {
-
             return `
-
-               <div style=' background: #0af58733;color: #0cb971; ' class="resultData">
+               <div
+               class=" resultAnim resultData"
+               style=' background: #0af58733;color: #0cb971;' >
                    <p>${imcResult.toFixed(2)}</p>
                   <span>Normal</span>
                </div>
@@ -57,16 +40,19 @@ function imcCalculator() {
 
             return `
 
-               <div style='background: #dbb61233;color: #dbb612;  ' class="resultData">
+               <div
+                  class="resultAnim resultData"
+                  style='background:#dbb61233; color:#dbb612;'
+               >
                    <p>${imcResult.toFixed(2)}</p>
-                  <span>Sobrepeso</span>
+                  <span>Acima do peso</span>
                </div>
             `
          } else if (imcResult >= 30 && imcResult <= 34.9) {
 
             return `
 
-               <div style=' background: #db8b1233;color: #db8b12; ' class="resultData">
+               <div style=' background: #db8b1233;color: #db8b12; ' class=" resultAnim resultData">
                     <p>${imcResult.toFixed(2)}</p>
                   <span>Obesidade 1</span>
                </div>
@@ -75,7 +61,7 @@ function imcCalculator() {
 
             return `
 
-               <div style=' background: #dd650333;color: #dd6503; ' class="resultData">
+               <div style=' background: #dd650333;color: #dd6503; ' class=" resultAnim resultData">
                   <p>${imcResult.toFixed(2)}</p>
                   <span>Obesidade 2</span>
                </div>
@@ -83,29 +69,39 @@ function imcCalculator() {
          } else if (imcResult > 40) {
             return `
 
-               <div style=' background: #ff330033;color: #ff3300; ' class="resultData">
+               <div style=' background: #ff330033;color: #ff3300; ' class=" resultAnim resultData">
                     <p>${imcResult.toFixed(2)}</p>
                   <span>Obesidade 3</span>
                </div>
             `
          } else {
 
-            return 'ERROR <br> Por favor digite um valor';
-         };
+            return `
+               <div class="resultData resultAnim" style='background: #aaa3;color: #555;'>
+                  <span>calcule para ver o resuldato</span>
+               </div>
+            `
+         }
+
+      };
+      function mobileResult() {
+
+         const ResultBubble = document.querySelector('.resultBubble');
+
+         ResultBubble.innerHTML = result();
+         ResultScreen.openResultScreen();
+      };
+      function deskResult() {
+
+         const resultDeskBubble = document.getElementById('resultDeskContainer');
+         resultDeskBubble.innerHTML = result();
       };
 
-      function finalResult() {
+      if (width.matches) {
 
-
-         const resultBubble = document.querySelector('.resultBubble');
-         const resultData = result();
-
-         resultBubble.innerHTML = resultData;
-
-         openResultScreen()
-      }
-
-      finalResult();
+         deskResult()
+      } else {
+         mobileResult();
+      };
    }
-
 }
